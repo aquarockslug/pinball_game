153cc246efc0f56a38eb44674cb999a9ac83c0c0
aquarock <aqua.rock.slug@gmail.com>
2024-04-06 15:49:54 -0500

    slingshot

--- static/javascripts/game.js
+++ static/javascripts/game.js
@@ -4,9 +4,9 @@ class Pinball extends Phaser.Scene {
 
 	preload() {
 		this.load.setBaseURL('assets/');
-		[['sky', 'sky.png'], ['board', 'board.png'], ['ball', 'silver_ball.png'],
-		['bumper', 'bumper_oval.png'], ['shade', 'shade.png'], 
-		['paddleLeft', 'paddleLeft.png'], ['paddleRight', 'paddleRight.png']
+		[['sky', 'sky.png'], ['board', 'board.png'], ['ball', 'silver_ball.png'], ['bumper', 'bumper_oval.png'], 
+		['shade', 'shade.png'], ['wall', 'wall.png'], ['paddleLeft', 'paddleLeft.png'], ['paddleRight', 'paddleRight.png'],
+		['slingshot', 'slingshot.png']
 		].forEach((asset) => this.load.image(asset[0], asset[1]))
 		this.load.json("sprites", "sprite-physics.json");
 	}
@@ -15,8 +15,11 @@ class Pinball extends Phaser.Scene {
 		this.add.image(pos.center.x, pos.center.y, 'sky').setScale(1.75)
 		this.add.image(pos.center.x, pos.center.y, 'board');
 		this.add.image(pos.center.x, pos.center.y, 'shade').setDepth(100)
+
+		this.createSlingshots()
 		this.createWalls()
 		this.createBumpers()
+		this.createSlingshots()
 		
 		this.matter.world.engine.positionIterations = 12 
 		this.matter.world.engine.velocityIterations = 8
@@ -24,47 +27,81 @@ class Pinball extends Phaser.Scene {
 		// this.matter.world.runner.deltaMax = 2 
 		// this.matter.world.runner.correction = 1 
 		// this.matter.world.runner.deltaMin = 1 
-		// this.matter.world.setGravity(0, 1, 0.001);
+		this.matter.world.setGravity(0, 1, 0.0015);
 		
 		this.paddles = newPaddles(this)
 		this.ball = newBall(this)
+		this.add.text(900, 75, 'Score: 0', { fontSize: '24px' });
 	}
 
 	update () {
 		this.updatePaddles()
+		this.handleCollision()
+		this.clamp()	
+		this.ballReturn()
 
 		// ball shrinks when higher on screen
-		this.ball.setScale(this.mapVal(this.ball.body.position.y, 500, 100, 0.4, 0.28))
+		this.ball.setScale(this.mapVal(this.ball.body.position.y, 500, 100, 0.35, 0.2))
+	}
 
-		// ball velocity clamp
+	clamp () {
 		if ( this.ball.body.velocity.y < -14 ) { this.ball.setVelocityY(-14) }
 		if ( this.ball.body.velocity.y > 14 ) { this.ball.setVelocityY(14) }
 		if ( this.ball.body.velocity.x < -14 ) { this.ball.setVelocityX(-14) }
 		if ( this.ball.body.velocity.x > 14 ) { this.ball.setVelocityX(14) }
+	}
 
-		// ball return
+	ballReturn () {
+		if ( this.ball.body.position.x > 600 && this.ball.body.position.y < 570)
+			this.ball.setVelocityY(-14)
+
 		if ( this.ball.body.position.y < 570) return
 		this.ball.setVelocityX(5)
 		this.input.keyboard.on('keydown', (event) => 
 			event.key == "ArrowUp" && this.ball.body.position.x > 600 ? 
-				this.ball.setVelocity(-3, -40) : null)
-	}
-	
-	createWalls () {
-		const wallSpread = 580
-		this.matter.add.rectangle(400, h - 80, 700, 50, 
-			{ isStatic: true, angle: Math.PI + Math.PI / 60 })
-		this.matter.add.rectangle(400, 40, 400, 50, { isStatic: true })
-		this.matter.add.rectangle(pos.board.x - wallSpread/2, pos.board.y, 50, h, 
-			{ isStatic: true, angle: Math.PI / 16 })
-		this.matter.add.rectangle(pos.board.x + wallSpread/2, pos.board.y, 50, h, 
-			{ isStatic: true, angle: -Math.PI / 16 })
+				this.ball.setVelocity(-1, -14) : null)
+
+	} 
+
+	handleCollision () {
+		this.matter.world.on("collisionstart", (event, bodyA, bodyB) => {
+			// console.log("collision start")
+        		if(bodyA.label == "bumper" && bodyB.label == "ball") {
+				console.log("bumper")	
+			}
+            	})
+        }
+
+	createSlingshots () {
+		const spread = 300
+		const spritePhysics = this.cache.json.get("sprites");
+		this.add.image(pos.paddle.x+spread/2, pos.paddle.y-100, 'slingshot', 
+			{ shape: spritePhysics['slingshot'] }).setScale(0.25)
+		this.add.image(pos.paddle.x-spread/2, pos.paddle.y-100, 'slingshot', 
+			{ shape: spritePhysics['slingshot'] }).setScale(0.25).setFlipX(true)
 	}
 
-	createBumpers(spread=250) {
-		this.newBumper(pos.board.x, pos.board.y-225, 0.4)
-		this.newBumper(pos.board.x+spread/2, pos.board.y-spread/2, 0.5)
-		this.newBumper(pos.board.x-spread/2, pos.board.y-spread/2, 0.5)
+	createWalls () {
+		// borders
+		const wallSpread = 530
+		this.matter.add.rectangle(pos.board.x, h - 100, 700, 30, 
+			{ isStatic: true, angle: Math.PI + Math.PI / 60 })
+		this.matter.add.rectangle(pos.board.x, 40, 400, 30, { isStatic: true })
+		this.matter.add.rectangle(pos.board.x - wallSpread/2, pos.board.y, 30, h, 
+			{ isStatic: true, angle: Math.PI / 16 })
+		this.matter.add.rectangle(pos.board.x + wallSpread/2, pos.board.y, 10, h, 
+			{ isStatic: true, angle: -Math.PI / 16 })
+
+		// on board
+		const spritePhysics = this.cache.json.get("sprites");
+		this.matter.add.sprite(pos.board.x + 200, pos.board.y-25, 
+			'wall', { shape: spritePhysics['wall'] }).setScale(1.0, 0.2).setStatic(true).setAngle(80)
+	}
+
+	createBumpers(spread=200) {
+		this.newBumper(pos.paddle.x, pos.paddle.y-325, 0.4)
+		this.newBumper(pos.paddle.x+spread/2, pos.board.y-spread/2-40, 0.5)
+		this.newBumper(pos.paddle.x-spread/2, pos.board.y-spread/2-40, 0.5)
 	}
 
 	newBumper(x, y, scale) {
@@ -115,7 +152,7 @@ const config = {
 		default: "matter",
 		matter: {
 			timing: {
-				timeScale: 1.5 
+				timeScale: 1 
 			},
 			// debug: true
 		}
@@ -133,16 +170,16 @@ const [w, h] = [config.width, config.height]
 pos = {
 	center: { x: w / 2, y: h / 2 },
 	launch: { x: 670, y:  600},
-	paddleCenter: { x: 395, y: h / 2 + 100 },
-	board: { x: 395, y: h / 2}
+	paddle: { x: 370, y: h / 2 + 100 },
+	board: { x: 385, y: h / 2}
 }
 
 function newBall(scene) {
-	const ball = scene.matter.add.image(pos.launch.x, pos.launch.y, 'ball');
-	return ball.setScale(0.4).setCircle(24).setBounce(0.5);
+	const ball = scene.matter.add.image(pos.launch.x, pos.launch.y, 'ball', null, { label: 'ball'});
+	return ball.setScale(0.3).setCircle(17).setBounce(0.75).setMass(0.00001);
 }
 
-function newPaddles(scene, center = pos.paddleCenter) {
+function newPaddles(scene, center = pos.paddle) {
 	const spritePhysics = scene.cache.json.get("sprites");
 	function fire(paddle, direction) { paddle.setAngularVelocity(0.15 * direction) }
 
@@ -157,10 +194,10 @@ function newPaddles(scene, center = pos.paddleCenter) {
 		rightFired: false
 	}
 
-	paddles.apply((paddle) => paddle.setScale(0.5))
+	paddles.apply((paddle) => paddle.setScale(0.35))
 	paddles.apply((paddle) => paddle.setFriction(0.0))
 
-	const options = { spread: 345, matter: {} }
+	const options = { spread: 250, matter: {} }
 	const leftOptions = { pointA: { x: center.x - options.spread/2, y: center.y}, ...options.matter}
 	const rightOptions = {  pointA: { x: center.x + options.spread/2, y: center.y}, ...options.matter}
 	scene.matter.add.worldConstraint(paddles.left, 0, 1.0, leftOptions)
